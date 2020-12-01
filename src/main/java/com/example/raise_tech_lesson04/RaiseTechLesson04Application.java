a1d09679b7dac88baac2ff29087154820f2175b8
TakTypeR <tak_type_r@hotmail.co.jp>
2020-12-01 23:32:06 +0900

    add RaiseTechLesson04Application.java

---
+++ src/main/java/com/example/raise_tech_lesson04/RaiseTechLesson04Application.java
@@ -0,0 +1,13 @@
+package com.example.raise_tech_lesson04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaiseTechLesson04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaiseTechLesson04Application.class, args);
+	}
+
+}
