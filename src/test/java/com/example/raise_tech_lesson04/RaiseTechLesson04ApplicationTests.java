8523f301d6315fbe234933133c3828f1dab1957e
TakTypeR <tak_type_r@hotmail.co.jp>
2020-12-07 22:28:23 +0900

    プロジェクトに関係するファイルを追加

---
+++ src/test/java/com/example/raise_tech_lesson04/RaiseTechLesson04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.raise_tech_lesson04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaiseTechLesson04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
