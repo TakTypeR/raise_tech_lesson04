514c63bbfa4f45655dc96d11f637e14cd58caf4e
TakTypeR <tak_type_r@hotmail.co.jp>
2021-01-11 22:24:20 +0900

    feat: 機材/個々の機材情報表示/編集の為の、ページ[machine_fomr.html]関連を作成開始 chore: bootstrap, jqueryのバージョン記述をしないように、pom.xmlにwebjars-locatorを追加

--- src/main/java/com/example/raise_tech_lesson04/controller/MachinesController.java
+++ src/main/java/com/example/raise_tech_lesson04/controller/MachinesController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.crypto.Mac;
 import java.util.List;
 
 @Controller
@@ -34,13 +35,24 @@ public class MachinesController {
         return "machines";
     }
 
-    //machines.htmlのhrefに対する受け
+    //machines.htmlのdelete::hrefに対する受け
     @GetMapping("/delete/{id}")
     // @PathVariable: URLに含まれるパラメータを取得する
-    public String DeleteMachine(@PathVariable("id") int id)
+    public String deleteMachine(@PathVariable("id") int id)
     {
         //更新したＤＢで持って、同ページを再表示
         machineInfoMapper.deleteById(id);
         return "redirect:/machines";
+    }
+
+    //machines.htmlのedit::hrefに対する受け
+    @GetMapping("/edit/{id}")
+    public String editMachine(@PathVariable("id") int id, Model model)
+    {
+        //遷移先のページで機材情報を表示する為、機材情報を取得して渡す
+        MachineInfo m = machineInfoMapper.findById(id);
+        model.addAttribute("machine", m);
+
+        return "/machine/machine_form";
     }
 }
