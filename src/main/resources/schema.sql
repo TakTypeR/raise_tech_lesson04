e0adde030047efc453cd31d9cdf5e43218bb852e
TakTypeR <tak_type_r@hotmail.co.jp>
2021-08-22 20:47:15 +0900

    refactor: DBのpcs2->pcsへ変更 refactor: DBから旧pcs削除に対応

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,13 +4,6 @@ CREATE TABLE IF NOT EXISTS platforms (
 );
 
 CREATE TABLE IF NOT EXISTS pcs (
-  id        INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
-  platform  VARCHAR(45)     NOT NULL,
-  host_name VARCHAR(45)     NOT NULL,
-  owner     VARCHAR(45)
-);
-
-CREATE TABLE IF NOT EXISTS pcs2 (
   id           INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
   platform_id  INT             NOT NULL,
   host_name    VARCHAR(45)     NOT NULL,
