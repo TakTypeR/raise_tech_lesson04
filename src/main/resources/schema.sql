4a1daeb681d05faa0b50d7c487137087513a8ca9
TakTypeR <tak_type_r@hotmail.co.jp>
2022-05-13 08:46:40 +0900

    Refactor: 未利用の為、機材情報のテーブル定義(pcs)からplatformを削除

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE IF NOT EXISTS pcs (
   id           INT             NOT NULL    AUTO_INCREMENT  PRIMARY KEY,
   platform_id  INT             NOT NULL,
   host_name    VARCHAR(45)     NOT NULL,
-  owner        VARCHAR(45),
-  platform     VARCHAR(45)     NOT NULL
+  owner        VARCHAR(45)
 );
+
