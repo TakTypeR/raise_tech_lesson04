3bc0b25f955aae8f544576196b5335812231e087
TakTypeR <tak_type_r@hotmail.co.jp>
2022-06-05 19:59:34 +0900

    Chore: Projectデバッグの為、プロパティファイルにログオプションを追加 Refactor: 不要なimportを削除

--- src/main/java/com/example/raise_tech_lesson04/service/ProjectService.java
+++ src/main/java/com/example/raise_tech_lesson04/service/ProjectService.java
@@ -1,9 +1,7 @@
 package com.example.raise_tech_lesson04.service;
 
-import com.example.raise_tech_lesson04.entity.MachineInfo;
 import com.example.raise_tech_lesson04.entity.Project;
 import com.example.raise_tech_lesson04.mapper.ProjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
